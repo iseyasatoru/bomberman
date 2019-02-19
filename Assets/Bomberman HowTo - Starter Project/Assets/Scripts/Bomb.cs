70aba9ac0a2d8b5b7f49cced0a9cf3fd5f865ed2
Satoru Iseya <satoruiseya0911@gmail.com>
2019-02-19 17:16:04 +0900

    first commit

---
+++ Assets/Bomberman HowTo - Starter Project/Assets/Scripts/Bomb.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public GameObject explosionPrefab;
+    public LayerMask levelMask;
+    private bool exploded = false;
+
+    void Start()
+    {
+        Invoke("Explode", 3f);
+
+        
+    }
+
+   
+    void Explode()
+    {
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        GetComponent<MeshRenderer>().enabled = false;
+        exploded = true;
+        StartCoroutine(CreateExplosions(Vector3.forward));
+        StartCoroutine(CreateExplosions(Vector3.left));
+        StartCoroutine(CreateExplosions(Vector3.right));
+        StartCoroutine(CreateExplosions(Vector3.back));
+
+
+
+        transform.Find("Collider").gameObject.SetActive(false);
+        Destroy(gameObject, 0.3f);
+
+    }
+    private IEnumerator CreateExplosions(Vector3 direction)
+    {
+        for (int i = 1; i < 3; i++) 
+        {
+            RaycastHit hit;
+            Physics.Raycast(
+                transform.position + new Vector3(0,0.5f,0),direction,out hit,i,levelMask
+            );
+            if (!hit.collider)
+            {
+                Instantiate(explosionPrefab, transform.position + (i * direction),explosionPrefab.transform.rotation);
+
+            }
+            else
+            {
+                break;
+
+            }
+            yield return new WaitForSeconds(0.05f);
+
+        }
+        
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if(!exploded && other.CompareTag("Explosion"))
+        {
+            CancelInvoke("Explode");
+
+            Explode();
+        }
+
+    }
+
+}
---
+++ Assets/Bomberman HowTo - Starter Project/Assets/Scripts/GlobalStateManager.cs
@@ -0,0 +1,80 @@
+﻿/*
+ * Copyright (c) 2017 Razeware LLC
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish, 
+ * distribute, sublicense, create a derivative work, and/or sell copies of the 
+ * Software in any work that is designed, intended, or marketed for pedagogical or 
+ * instructional purposes related to programming, coding, application development, 
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works, 
+ * or sale is expressly withheld.
+ *    
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class GlobalStateManager : MonoBehaviour
+{
+    private int deadPlayerNumber = -1;
+    private int  deadPlayers = 0;
+    public GameObject score;
+
+    public void Start()
+    {
+        this.score = GameObject.Find("Score");
+
+    }
+
+    public void PlayerDied (int playerNumber)
+    {
+        deadPlayers++;
+        if(deadPlayers == 1)
+        {
+            deadPlayerNumber = playerNumber;
+            Invoke("CheckPlayersDeath", 0.3f);
+
+        }
+    }
+    void CheckPlayersDeath()
+    {
+        if (deadPlayers == 1)
+        {
+            if(deadPlayerNumber == 1)
+            {
+                this.score.GetComponent<Text>().text = "プレイヤー1(青)の勝利！";
+            }
+            else
+            {
+                this.score.GetComponent<Text>().text = "プレイヤー2(赤)の勝利！";
+
+            }
+        }
+        else
+        {
+            this.score.GetComponent<Text>().text = "引き分け";
+        }
+
+
+
+    }
+}
